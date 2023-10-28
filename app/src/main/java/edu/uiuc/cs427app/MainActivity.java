2d35aab7e5089c67c5cac0cd4c7ea1d275050826
sriramr4 <sriramr4@illinois.edu>
2023-10-28 11:47:46 -0400

    MainActivity Changes
    Added support to add/remove city for the user

--- app/src/main/java/edu/uiuc/cs427app/MainActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/MainActivity.java
@@ -1,10 +1,13 @@
 package edu.uiuc.cs427app;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.navigation.ui.AppBarConfiguration;
@@ -13,14 +16,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import edu.uiuc.cs427app.Database.AppDatabase;
 import edu.uiuc.cs427app.Database.Entity.City;
+import edu.uiuc.cs427app.Database.Entity.SavedCity;
+import edu.uiuc.cs427app.Helper.AlertHelper;
 import edu.uiuc.cs427app.Helper.SharedPrefUtils;
 import edu.uiuc.cs427app.Helper.ThemeHelper;
 import edu.uiuc.cs427app.databinding.ActivityMainBinding;
 
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends BaseActivity  {
@@ -33,7 +40,7 @@ public class MainActivity extends BaseActivity  {
     public void onResume(){
         super.onResume();
         //TODO
-        //rv_city_list.setAdapter(new Adapter());
+        rv_city_list.setAdapter(new CustomAdapter(SharedPrefUtils.getIntData(this, "userid")));
     }
 
     @Override
@@ -46,7 +53,6 @@ public class MainActivity extends BaseActivity  {
         btn_buttonAddLocation = findViewById(R.id.buttonAddLocation);
         rv_city_list = findViewById(R.id.city_list);
         rv_city_list.setLayoutManager(new LinearLayoutManager(this));
-        //greeting.setText(greeting.getText().toString().replace("{username}",SharedPrefUtils.getStringData(this,"username")));
 
         iv_setting.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +82,56 @@ public class MainActivity extends BaseActivity  {
                 MainActivity.this.startActivity(new Intent(MainActivity.this, AddCityActivity.class));
             }
         });
+    }
+
+
+    public class CustomAdapter extends RecyclerView.Adapter<MainActivity.CustomAdapter.ViewHolder> {
+        List<City> localDataSet = new ArrayList<>();
+
+        public  class ViewHolder extends RecyclerView.ViewHolder {
+            private TextView city_name;
+            private Button btn_delete;
+
+            public ViewHolder(View view) {
+                super(view);
+                city_name = (TextView) view.findViewById(R.id.city_name);
+                btn_delete = view.findViewById(R.id.delete);
+            }
+
+            public TextView getTextView() {
+                return city_name;
+            }
+        }
+        public CustomAdapter(int userid) {
+            localDataSet = AppDatabase.getAppDatabase(MainActivity.this).savedCityDao().loadCityByUserId(userid);
+        }
+
+        @NonNull
+        @Override
+        public MainActivity.CustomAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.recycler_view_citylist_row, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MainActivity.CustomAdapter.ViewHolder viewHolder, @SuppressLint("RecyclerView") final int position) {
+            viewHolder.getTextView().setText(localDataSet.get(position).getCityName().toString());
+            viewHolder.btn_delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int userid = SharedPrefUtils.getIntData(MainActivity.this, "userid");
+                    SavedCity savedCity = AppDatabase.getAppDatabase(MainActivity.this).savedCityDao().isCityExistByUserId(userid, localDataSet.get(position).getId());
+                    AppDatabase.getAppDatabase(MainActivity.this).savedCityDao().delete(userid, localDataSet.get(position).getId());
+                    localDataSet = AppDatabase.getAppDatabase(MainActivity.this).savedCityDao().loadCityByUserId(userid);
+                    CustomAdapter.this.notifyDataSetChanged();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return localDataSet.size();
+        }
     }
 }
 
