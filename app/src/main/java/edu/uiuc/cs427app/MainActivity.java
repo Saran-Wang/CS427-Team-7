f342999356939071c6a4e2eea20d6082c6645b05
hmyu2 <hmyu2@illinois.edu>
2023-11-05 05:47:06 +0800

    makes weather button works in main pae

--- app/src/main/java/edu/uiuc/cs427app/MainActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/MainActivity.java
@@ -105,15 +105,20 @@ public class MainActivity extends BaseActivity {
          */
         public class ViewHolder extends RecyclerView.ViewHolder {
             private TextView city_name;
+            private Button weather_button, map_button;
             private Button btn_delete;
 
             /**
-            * Constructor for the ViewHolder.
-            * @param view The View object representing an item in the RecyclerView.
-            */
+            * Constructor for the ViewHolder.* @param view The View object representing an item in the RecyclerView.
+             */
+            public Button getWeather_button() {return weather_button;}
+            public Button getMapButton() {return map_button;}
+
             public ViewHolder(View view) {
                 super(view);
                 city_name = (TextView) view.findViewById(R.id.city_name);
+                weather_button = view.findViewById(R.id.weather_button);
+                map_button = view.findViewById(R.id.map_button);
                 btn_delete = view.findViewById(R.id.delete);
             }
 
@@ -155,6 +160,12 @@ public class MainActivity extends BaseActivity {
         public void onBindViewHolder(MainActivity.CustomAdapter.ViewHolder viewHolder, @SuppressLint("RecyclerView") final int position) {
             viewHolder.getTextView().setText(localDataSet.get(position).getCityName().toString());
             //This line of code will set the text of the TextView in the ViewHolder to the city name at the given position in the dataset
+            (viewHolder.getWeather_button()).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    MainActivity.this.startActivity(new Intent(MainActivity.this, WeatherActivity.class).putExtra("city_name", localDataSet.get(position).getCityName()).putExtra("lat", localDataSet.get(position).getLat() + "").putExtra("log", localDataSet.get(position).getLog() + ""));
+                }
+            });
             viewHolder.btn_delete.setOnClickListener(new View.OnClickListener()
                     //This line of code will create a button, when clicked the button will execute the code inside the method onClick
             {
