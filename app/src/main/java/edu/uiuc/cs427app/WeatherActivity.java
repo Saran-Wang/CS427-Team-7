21e3327b01b4e501808a4dde213acb7b9b3bc5e4
hmyu2 <hmyu2@illinois.edu>
2023-11-05 12:59:49 +0800

    remove unused import

--- app/src/main/java/edu/uiuc/cs427app/WeatherActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/WeatherActivity.java
@@ -1,30 +1,9 @@
 package edu.uiuc.cs427app;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
-
-import edu.uiuc.cs427app.Database.AppDatabase;
-import edu.uiuc.cs427app.Database.Entity.User;
-import edu.uiuc.cs427app.Helper.AlertHelper;
-import edu.uiuc.cs427app.Helper.SharedPrefUtils;
-import edu.uiuc.cs427app.Network.Entity.Forecast;
 
 public class WeatherActivity extends BaseActivity {
     private String ENDPOINT = "https://api.open-meteo.com/v1/forecast?latitude=_lat_&longitude=_log_&current=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m,wind_direction_10m&timezone=auto";
